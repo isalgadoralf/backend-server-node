2b2ff324835d1212aa07a9f4c93f110e277c91eb
Rafael Salgado <isalgadoralf@gmail.com>
2019-01-13 11:19:07 -0400

    frist commint

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+// Requires
+var express = require('express');
+var mongoose = require('mongoose');
+
+// Inicialzar variables
+
+var app  = express();
+
+//Conexion a la base de datos
+
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error,res)=>{
+
+    if( error ) throw error;
+
+    console.log('Base de datos :\x1b[32m%s\x1b[0m',' online');
+
+})
+
+
+
+//Rutas
+
+// app.get('/', (req,res,next ) );
+ app.get('/', (req,res,next ) => {
+
+    res.status(200).json({
+       ok:true,
+       mensaje:'Peticion realizad correctamente'
+    });
+
+ });
+
+
+//Escuchar peticiones
+
+app.listen(3000,()=>{
+   console.log('Express server puerto 3000:\x1b[32m%s\x1b[0m',' online');
+});
